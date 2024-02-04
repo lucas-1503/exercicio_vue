1ff60a01f11af38fc0f0f693f4f296bf4550d8b7
Lucas Montezuma <lucas.montezumaa@gmail.com>
2024-02-04 13:23:16 -0300

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,24 @@
 <script setup>
 
-var resposta = new Vue({
-  el: '#container',
-  data: {
-    resposta: 0,
-    num1: 0,
-    num2: 0,
-  },
-  methods: {
-    somarNumeros: function() {
-      this.resposta = num1 + num2
-    },
-    subtrairNumeros: function(){
-      this.resposta = num1 - num2
-    },
-    dividirNumeros: function(){
-      this.resposta = num1/num2
-    },
-    multiplicarNumeros: function(){
-      this.resposta = num1*num2
-    },
-  },
-})
+import { ref } from 'vue';
+
+const resposta = ref(0);
+const num1 = ref(0);
+const num2 = ref(0);
+
+function somarNumeros () {
+  resposta.value = Number(num1.value) + Number(num2.value);
+}
+
+function subtrairNumeros () {
+  resposta.value = Number(num1.value) - Number(num2.value);
+}
+function dividirNumeros () {
+  resposta.value = Number(num1.value) / Number(num2.value);
+}
+function multiplicarNumeros () {
+  resposta.value = Number(num1.value) * Number(num2.value);
+}
 
 </script>
 
@@ -30,8 +27,8 @@ var resposta = new Vue({
     <header class="p-5 mb-4 mt-4 bg-ligth rounded-3 ">
       <h1>Calculadora</h1>
     </header>
-    <input id="num1" value:`$(num1)` class="me-2" type="number" placeholder="Primeiro número">
-    <input id="num2" value:`$(num2)` type="number" placeholder="Segundo número">
+    <input v-model="num1"  class="me-2" type="number" placeholder="Primeiro número">
+    <input v-model="num2" type="number" placeholder="Segundo número">
     <br>
     <div class="operadores">
       <button v-on:click ="somarNumeros" class="btn btn-primary m-2 fw-bold ">+</button>
@@ -39,7 +36,7 @@ var resposta = new Vue({
       <button v-on:click ="dividirNumeros" class="btn btn-primary m-2 fw-bold" type="submit">/</button>
       <button v-on:click ="multiplicarNumeros" class="btn btn-primary m-2 fw-bold" type="submit">*</button>
     </div>
-    <h3>Resposta: {{ resposta.value }}</h3>
+    <h3>Resposta: {{ resposta }}</h3>
   </div>
 </template>
 
